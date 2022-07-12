57681412c08e9127c5a56f0f6b50efd42d10e334
matcox <matthijs.cox@asml.com>
2022-07-12 15:10:08 +0200

    julia-build in Makefile

--- basic/BasicTypes/build/build.jl
+++ basic/BasicTypes/build/build.jl
@@ -1,18 +1,19 @@
-import PackageCompiler, TOML
+import PackageCompiler, TOML, Pkg
 
 if length(ARGS) < 1
-    target_dir = realpath(joinpath(@__DIR__, "../../BasicTypesCompiled"))
+    target_dir = abspath(joinpath(@__DIR__, "../../BasicTypesCompiled"))
 else
     target_dir = ARGS[1]
 end
 println("Creating library in $target_dir")
 
 const build_dir = @__DIR__
-const project_toml = realpath(joinpath(build_dir, "..", "Project.toml"))
+const package_dir = joinpath(build_dir, "..")
+const project_toml = realpath(joinpath(package_dir, "Project.toml"))
 const version = VersionNumber(TOML.parsefile(project_toml)["version"])
 
 PackageCompiler.create_library(
-    ".", target_dir;
+    package_dir, target_dir;
     lib_name="basic",
     precompile_execution_file=[joinpath(build_dir, "generate_precompile.jl")],
     #precompile_statements_file=[joinpath(build_dir, "additional_precompile.jl")],
