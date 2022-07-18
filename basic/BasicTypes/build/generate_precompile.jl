981c27e6e99d7fc4a090262298725ba9b5f637b0
Matthijs Cox <matthijs.cox@asml.com>
2022-07-18 16:16:40 +0200

    add reminder for julia -h

--- basic/BasicTypes/build/generate_precompile.jl
+++ basic/BasicTypes/build/generate_precompile.jl
@@ -1,6 +1,8 @@
 # for testing, cd into BasicTypes folder and run as follows:
 # julia --startup-file=no --project=. build/generate_precompile.jl
 
+# Note: to see help for Julia flags use julia -h, or julia --help-hidden.
+
 using BasicTypes
 
 # enable debug printing
